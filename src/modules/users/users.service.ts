8dcfb3a966a89d3315ce71dfc041d25b4a055d40
Mateo7722 <mateor861@gmail.com>
2024-11-21 16:36:20 -0300

    Fix: Bugs en user service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma/prisma.service';
@@ -7,96 +12,115 @@ import { REQUEST } from '@nestjs/core';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService, @Inject(REQUEST) private readonly request: Request){}
+  constructor(
+    private prisma: PrismaService,
+    @Inject(REQUEST) private readonly request: Request,
+  ) {}
 
-  async create(createUserDto: CreateUserDto) : Promise<CreateUserDto> {
-    const {email} = createUserDto
+  async create(createUserDto: CreateUserDto): Promise<CreateUserDto> {
+    const { email } = createUserDto;
 
     //Hasheo la contraseña
-    createUserDto.password = await hashPassword(createUserDto.password)
-    
+    createUserDto.password = await hashPassword(createUserDto.password);
+
     //Verifico si el usuario existe
-    const user = await this.prisma.user.findUnique({where: {email}})
-    if(!user.deletedAt) throw new BadRequestException('User Already Exists')
-    
-    //Si el usuario tiene un deletedAt quiere decir que fue borrado
-    //Osea que puedo usar su correo para crear otro usuario
-    await this.prisma.user.delete({where: {email: user.email}})
+    // Verifico si el usuario existe
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (user) {
+      if (!user.deletedAt) {
+        throw new BadRequestException('User Already Exists');
+      }
+      //Si el usuario tiene un deletedAt quiere decir que fue borrado
+      //Osea que puedo usar su correo para crear otro usuario
+      await this.prisma.user.delete({ where: { email: user.email } });
+    }
 
     //Creo el usuario
-    return await this.prisma.user.create({data: {...createUserDto}})
+    return await this.prisma.user.create({ data: { ...createUserDto } });
   }
 
-  async findAll() :  Promise<CreateUserDto[]> {
-    const users = await this.prisma.user.findMany({where: {deletedAt: null}})
+  async findAll(): Promise<CreateUserDto[]> {
+    const users = await this.prisma.user.findMany({
+      where: { deletedAt: null },
+    });
 
     //Verifico si hay usuarios
-    if(users.length === 0) throw new NotFoundException('Could Not Find Users')
+    if (users.length === 0) throw new NotFoundException('Could Not Find Users');
 
     return users;
   }
 
-  async findOne(id: number, getDeletes?: boolean) : Promise<CreateUserDto>{
+  async findOne(id: number, getDeletes?: boolean): Promise<CreateUserDto> {
     const where = { id, deletedAt: null };
     if (getDeletes) delete where['deletedAt'];
-    const user = await this.prisma.user.findFirst({where});
+    const user = await this.prisma.user.findFirst({ where });
     if (!user) throw new NotFoundException('User Not Found');
 
     return user;
   }
 
-  async findOneByEmail(email: string, getDeletes?: boolean) : Promise<CreateUserDto>{
+  async findOneByEmail(
+    email: string,
+    getDeletes?: boolean,
+  ): Promise<CreateUserDto> {
     const where = { email, deletedAt: null };
     if (getDeletes) delete where['deletedAt'];
-    const user = await this.prisma.user.findFirst({where});
+    const user = await this.prisma.user.findFirst({ where });
     if (!user) throw new NotFoundException('User Not Found');
 
     return user;
   }
 
-  async update(id: number, updateUserDto: Partial<UpdateUserDto>, getDeletes?: boolean) {
+  async update(
+    id: number,
+    updateUserDto: Partial<UpdateUserDto>,
+    getDeletes?: boolean,
+  ) {
     const user = await this.findOne(id, getDeletes);
 
     //Evito errores cuando se utiliza el mismo email dos veces
-    if(updateUserDto.email !== undefined){
-      const isUsed = await this.prisma.user.findFirst({where: {email: updateUserDto.email}})
-      if(isUsed) throw new BadRequestException('Email is already used')
+    if (updateUserDto.email !== undefined) {
+      const isUsed = await this.prisma.user.findFirst({
+        where: { email: updateUserDto.email },
+      });
+      if (isUsed) throw new BadRequestException('Email is already used');
     }
 
-    return await this.prisma.user.update({ where: { id }, data: updateUserDto });
+    return await this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
   }
 
   async changeToAdmin(id: number) {
-    const user = await this.update(id, { role: "admin" }, false);
+    const user = await this.update(id, { role: 'admin' }, false);
     return user;
   }
 
   async changeToUser(id: number) {
-    const user = await this.update(id, { role: "user" }, false);
+    const user = await this.update(id, { role: 'user' }, false);
     return user;
   }
 
   async changeRole(id: number) {
-    const user = await this.findOne(id)
-  
-    if(user.role === 'admin'){
-      const newUser = this.update(id, {role: 'user'})
-      return newUser
-    }else if(user.role === 'user'){
-      const newUser = await this.update(id, {role: 'admin'})
-      return newUser
-    }else{
-      throw new BadRequestException('Superadmin cant change role')
+    const user = await this.findOne(id);
+
+    if (user.role === 'admin') {
+      const newUser = this.update(id, { role: 'user' });
+      return newUser;
+    } else if (user.role === 'user') {
+      const newUser = await this.update(id, { role: 'admin' });
+      return newUser;
+    } else {
+      throw new BadRequestException('Superadmin cant change role');
     }
-  
   }
 
-
   async remove(id: number) {
-    const user = await this.findOne(id, false)
+    const user = await this.findOne(id, false);
 
-    if(user.role === 'superadmin'){
-      throw new BadRequestException('Superadmin cant be deleted')
+    if (user.role === 'superadmin') {
+      throw new BadRequestException('Superadmin cant be deleted');
     }
 
     await this.update(id, { deletedAt: new Date() });
@@ -107,8 +131,4 @@ export class UsersService {
     const user = await this.update(id, { deletedAt: null }, true);
     return user;
   }
-
-  
-
-
 }
